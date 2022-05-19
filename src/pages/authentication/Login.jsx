17940cde3f41ba0ae787204c6c3082057dada5aa
Meghana-sk <meghana.meghanask.sk@gmail.com>
2022-05-20 00:08:17 +0530

    feat: Add Login and Signup screens

--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -1,5 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import "./authentication.css";
 const Login = () => {
-  return <p>Note</p>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <form className="auth-container">
+        <h2 className="white-font">Login</h2>
+        <label htmlFor="email" className="text-left white-font">
+          Email
+        </label>
+        <input
+          name="email"
+          id="email"
+          type="text"
+          placeholder="meghana@yahoo.com"
+          className="input-text"
+          required
+        />
+        <label htmlFor="password" className="text-left white-font">
+          Password
+        </label>
+        <input
+          name="password"
+          id="password"
+          type="password"
+          className="input-text"
+          placeholder="********"
+          required
+        />
+        <button className="btn btn-primary">Login</button>
+        <button className="btn btn-secondary">Login as guest</button>
+        <p className="white-font">Don't have an account?</p>
+        <button
+          className="btn btn-secondary-text text-s white-font signup-btn"
+          onClick={() => {
+            navigate("/signup");
+          }}
+        >
+          Sign up
+        </button>
+      </form>
+    </>
+  );
 };
 
 export { Login };
--- src/pages/authentication/Signup.jsx
+++ src/pages/authentication/Signup.jsx
@@ -1,5 +1,81 @@
+import { useNavigate } from "react-router-dom";
+import "./authentication.css";
 const Signup = () => {
-  return <p>Note</p>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <form className="auth-container">
+        <h2 className="white-font">Signup</h2>
+        <label htmlFor="first-name" className="text-left white-font">
+          First name
+        </label>
+        <input
+          name="first-name"
+          id="first-name"
+          type="text"
+          placeholder="Michael"
+          className="input-text"
+          required
+        />
+        <label htmlFor="last-name" className="text-left white-font">
+          Last name
+        </label>
+        <input
+          name="last-name"
+          id="last-name"
+          type="text"
+          placeholder="Jackson"
+          className="input-text"
+          required
+        />
+        <label htmlFor="email" className="text-left white-font">
+          Email
+        </label>
+        <input
+          name="email"
+          id="email"
+          type="text"
+          placeholder="mj@yahoo.com"
+          className="input-text"
+          required
+        />
+        <label htmlFor="password" className="text-left white-font">
+          Password
+        </label>
+        <input
+          name="password"
+          id="password"
+          type="password"
+          className="input-text"
+          placeholder="********"
+          required
+        />
+        <label htmlFor="confirm-password" className="text-left white-font">
+          Confirm Password
+        </label>
+        <input
+          name="password"
+          id="confirm-password"
+          type="password"
+          className="input-text"
+          placeholder="********"
+          required
+        />
+        <button className="btn btn-primary" type="submit">
+          Signup
+        </button>
+        <p className="white-font">Already have an account?</p>
+        <button
+          className="btn btn-secondary-text text-s white-font signup-btn"
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          Login
+        </button>
+      </form>
+    </>
+  );
 };
 
 export { Signup };
